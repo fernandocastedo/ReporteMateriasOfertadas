518eca46bcf7403161f4216310fa6d284c2b148a
fernandocastedo <cmpfernando.ouac@gmail.com>
2025-06-11 15:04:37 -0400

    Add project files.

---
+++ ReporteMateriasOfertadas/gdatos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReporteMateriasOfertadas
+{
+    public class gdatos
+    {
+        private string cadenaConexion = @"Data Source=FERCASTEDO;Initial Catalog=UniversidadDB;Integrated Security=True";
+
+        public DataTable EjecutarConsulta(string consulta)
+        {
+            using (SqlConnection con = new SqlConnection(cadenaConexion))
+            {
+                SqlDataAdapter da = new SqlDataAdapter(consulta, con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+            }
+        }
+
+        public void EjecutarComando(string comando)
+        {
+            using (SqlConnection con = new SqlConnection(cadenaConexion))
+            {
+                SqlCommand cmd = new SqlCommand(comando, con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
